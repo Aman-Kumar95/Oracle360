678303c13991e03d755127ecf1e813590bb5cabe
Aman Kumar95 <amansd349@gmail.com>
2026-04-06 22:26:49 +0530

    sentimental analaysis

--- dataextract.py
+++ dataextract.py
@@ -12,3 +12,24 @@ params= {"q":query}
 headers= {
     "User-Agent" : "Mozilla/5.0"
 }
+
+response = requests.get(url,headers= headers,params=params)
+soup= BeautifulSoup(response.text,"html.parser")
+
+posts= soup.find_all("a",class_= "title")
+data= []
+
+for post in posts[:20]:
+    title= post.text
+    link = post.get("href")
+
+    polarity= TextBlob(title).sentiment.polarity
+
+    if polarity>0:
+        sentiment= "Positive"
+    elif polarity <0 :
+        sentiment= "Negative"
+    else:
+        sentiment= "Neutral"
+
+        data.append([title,link,sentiment])
